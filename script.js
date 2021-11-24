debffdd20cd0b34e685d5dc7455ba38e2289a37c
bonhom_j <bonhom_j@etna-alternance.net>
2021-11-24 10:51:16 +0100

    Minions ajoutés et fonction clickFish modifiée

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Étape 1
 
 let fish = 0;
-let fps = 1;
+let fps = 0;
 let clickValue = 1;
 
 function addFish(x) {
@@ -16,10 +16,7 @@ function displayFish() {
 }
 
 function fishPerSec() {
-    setInterval(function() {
-        addFish(fps);
-        displayFish();
-    }, 1000);
+    setInterval(clickFish(fps), 1000);
 }
 
 function clickFish(clickValue) {
@@ -29,3 +26,15 @@ function clickFish(clickValue) {
 }
 
 fishPerSec();
+
+//Étape 2 - Minions
+
+var minous = [
+    {id:1, name:"cat", cost:10, fps:0.1, owned: 0},
+    {id:2, name:"fisher", cost:100, fps:1, owned: 0},
+    {id:3, name:"boat", cost:1000, fps:10, owned: 0},
+    {id:4, name:"net", cost:10000, fps:100, owned: 0},
+    {id:5, name:"swimmer", cost:100000, fps:1000, owned: 0},
+    {id:6, name:"mermaid", cost:1000000, fps:10000, owned: 0},
+    {id:7, name:"poseicat", cost:10000000, fps:100000, owned: 0},
+]
