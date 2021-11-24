61bbc776e22a81cbe4b3c3d9fb06cafde6f538d5
Scotty <scotty@LAPTOP-10M9BL7E.localdomain>
2021-11-24 10:42:07 +0100

    Ajout gps

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 //Étape 1
 
 let fish = 0;
+let fps = 1;
+let clickValue = 1;
 
 function addFish(x) {
     fish += x;
 }
 
 function displayFish() {
-    const tag = document.querySelector('.fishN');
-    tag.innerHTML = "Fish : "+fish;
+    const fishN = document.querySelector('.fishN');
+    fishN.innerHTML = "Fish : "+fish;
+    const fishS = document.querySelector('.fishS');
+    fishS.innerHTML = "Fish/Second : "+fps;
 }
 
-function clickFish() {
+function fishPerSec() {
+    setInterval(function() {
+        addFish(fps);
+        displayFish();
+    }, 1000);
+}
+
+function clickFish(clickValue) {
     //Fonction appelée quand on clique sur le poisson
-    addFish(1);
+    addFish(clickValue);
     displayFish();
 }
+
+fishPerSec();
