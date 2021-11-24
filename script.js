e504d36c30d33757b1b75dd9000329c8177c259d
Scotty <scotty@LAPTOP-10M9BL7E.localdomain>
2021-11-24 11:35:00 +0100

    getGPS implémentée

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ let fish = 0;
 let fps = 0;
 let clickValue = 0;
 
+
+
 function addFish(x) {
     fish += x;
 }
 
 function displayFish() {
     const fishN = document.querySelector('.fishN');
-    fishN.innerHTML = "Fish : "+fish;
+    fishN.innerHTML = "Fish : " + Math.floor(fish);
     const fishS = document.querySelector('.fishS');
-    fishS.innerHTML = "Fish/Second : "+fps;
+    fishS.innerHTML = "Fish/Second : " + fps;
+    const fishC = document.querySelector('.fishC');
+    fishC.innerHTML = "Fish/Click : " + clickValue;
 }
 
 function fishPerSec() {
-    setInterval(clickFish(fps), 1000);
+    setInterval(clickFish, 1000, fps);
 }
 
 function clickFish(clickValue) {
@@ -25,12 +29,10 @@ function clickFish(clickValue) {
     displayFish();
 }
 
-fishPerSec();
-
 //Étape 2 - Minions
 
 var minous = [
-    {id:1, name:"cathand", cost:10, clickvalue:1, owned: 1},
+    {id:1, name:"cathand", cost:10, clickValue:1, owned: 1},
     {id:2, name:"cat", cost:10, fps:0.1, owned: 0},
     {id:3, name:"fisher", cost:100, fps:1, owned: 0},
     {id:4, name:"catcher", cost:1000, fps:10, owned: 0},
@@ -39,3 +41,21 @@ var minous = [
     {id:7, name:"mermaid", cost:1000000, fps:10000, owned: 0},
     {id:8, name:"poseicat", cost:10000000, fps:100000, owned: 0},
 ]
+
+let total = 0;
+function getFps() {
+    total = 0;
+    minous.forEach(minou => getValues(minou));
+    fps = total;
+}
+
+function getValues(minou) {
+        if (minou.id == 1) {
+            clickValue = minou.owned * minou.clickValue;
+        } else {
+            total += minou.owned * minou.fps;
+        }
+}
+
+getFps();
+fishPerSec();
